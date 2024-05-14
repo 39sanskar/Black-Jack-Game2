f18712d9e7f0705172f4bf18ced8e175fe344235
39sanskar <mishrasanskar496@gmail.com>
2024-05-14 23:35:36 +0530

    Add some features in javascript

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 let firstCard = 10
 let secondCard = 4
+let cards = [ firstCard, secondCard ] // array- ordered lists 
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-let messageEl = document.getElementById("mesahe-el")
-console.log(messageEl)
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+//let sumEl = document.querySelector(".sum-el")
+let cardsEl = document.getElementById("cards-el")
 function startGame(){
+    cardsEl.textContent = "Cards:" + firstCard +" "+ secondCard
+    sumEl.textContent = "Sum:" + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card? 🙂"
     } else if (sum === 21) {
@@ -17,4 +22,16 @@ function startGame(){
         isAlive = false
     }
     console.log(message)
+    messageEl.textContent = message
+    
+}
+function newCard(){
+    console.log("Drawing a new card from the deck!")
+     // 1. Create a card variable, and hard code its value to a number (2-11)
+     let card = 7
+     // 2. Add the new card to the sum variable
+     sum += card
+     // 3. Call startGame()
+     startGame()
+
 }
